3ddff3e6ac9859b8a74c33000cb6a7b23ca64912
Navneet852 <78260839+Navneet852@users.noreply.github.com>
2023-03-05 21:32:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let svg = document.getElementById("svg")
+let sv=document.getElementById("sv")
+svg.addEventListener("click", function () {
+    // e.preventDefault;
+    // svg.classList.remove("id");
+    // void svg.offsetWidth;
+    // svg.classList.add("id")
+    sv.style.animation = "dash 3s ease-out 1 forwards";
+});
+let svg2 = document.getElementById("svg2")
+svg2.addEventListener("click", function () {
+    sv2.style.animation = "dash 3s ease-out 1 forwards";
+});
+let svg3 = document.getElementById("svg3")
+svg3.addEventListener("click", function () {
+    sv3.style.animation = "dash 3s ease-out 1 forwards";
+});
+let svg4 = document.getElementById("svg4")
+svg4.addEventListener("click", function () {
+    sv4.style.animation = "dash 3s ease-out 1 forwards";
+});
+let svg5 = document.getElementById("svg5")
+svg5.addEventListener("click", function () {
+    sv5.style.animation = "dash 3s ease-out 1 forwards";
+});
+let svg6 = document.getElementById("svg6")
+svg6.addEventListener("click", function () {
+    sv6.style.animation = "dash 3s ease-out 1 forwards";
+});
